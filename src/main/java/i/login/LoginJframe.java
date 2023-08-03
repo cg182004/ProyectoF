ce92729c73654be401b6c51fae1bcffd72caaac3
Davidson <Dalvinsonreyes.p@gmail.com>
2023-08-03 00:10:54 -0400

    CAMBIOS V1

--- src/main/java/i/login/LoginJframe.java
+++ src/main/java/i/login/LoginJframe.java
@@ -225,6 +225,7 @@ private boolean isValidCredentials(String username, String password) {
     public static void main(String args[]) {
       
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new LoginJframe().setVisible(true);
             }
